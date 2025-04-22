2db12340102cfc147a2fcd296d558d1827eb9db0
sbx_user1051 <sbx_user1051@169.254.13.41>
2025-04-22 21:35:34 +0000

    Tue Apr 22 21:35:34 UTC 2025

--- src/BeFaster.App/Solutions/HLO/HelloSolution.cs
+++ src/BeFaster.App/Solutions/HLO/HelloSolution.cs
@@ -6,7 +6,8 @@ namespace BeFaster.App.Solutions.HLO
     {
         public string Hello(string? friendName)
         {
-            throw new SolutionNotImplementedException();
+            return $"Hello {friendName}";
         }
     }
 }
+

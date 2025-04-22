892bc443d701ec7f7fd44879655df340c16b6a70
sbx_user1051 <sbx_user1051@169.254.14.255>
2025-04-22 22:04:51 +0000

    Tue Apr 22 22:04:51 UTC 2025

--- src/BeFaster.App/Solutions/CHK/CheckoutSolution.cs
+++ src/BeFaster.App/Solutions/CHK/CheckoutSolution.cs
@@ -6,7 +6,33 @@ namespace BeFaster.App.Solutions.CHK
     {
         public int Checkout(string? skus)
         {
-            throw new SolutionNotImplementedException();
+            if (skus == null)
+                return -1;
+
+            var itemDictionary = new Dictionary<Char, int>()
+            {
+                {'A', 0},
+                {'B', 0},
+                {'C', 0},
+                {'D', 0}
+            };
+
+            foreach (var sku in skus)
+            {
+                if (itemDictionary.ContainsKey(sku))
+                    itemDictionary[sku]++;
+                else
+                    return -1;
+            }
+
+            var total = itemDictionary['C'] * 20 + itemDictionary['D'] * 15;
+            if (itemDictionary['A'] >= 3)
+                total += (itemDictionary['A'] % 3) * 50 + (itemDictionary['A'] / 3) * 130;
+            if (itemDictionary['B'] >= 2)
+                total += (itemDictionary['B'] % 2) * 30 + (itemDictionary['B'] / 2) * 45;
+            
+            return total;
         }
     }
 }
+

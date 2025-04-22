5750b95086ec5ae1fce87a64d28f3fc53316e97f
sbx_user1051 <sbx_user1051@169.254.13.41>
2025-04-22 21:31:35 +0000

    Tue Apr 22 21:31:35 UTC 2025

--- src/BeFaster.App/Solutions/SUM/SumSolution.cs
+++ src/BeFaster.App/Solutions/SUM/SumSolution.cs
@@ -1,12 +1,11 @@
-﻿using BeFaster.Runner.Exceptions;
-
-namespace BeFaster.App.Solutions.SUM
+﻿namespace BeFaster.App.Solutions.SUM
 {
     public class SumSolution
     {
         public int Sum(int x, int y)
         {
-            throw new SolutionNotImplementedException();
+            return x + y;
         }
     }
 }
+

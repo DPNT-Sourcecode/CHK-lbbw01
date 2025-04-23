25ce21a9dc1662c1745d8072c3e15d42750b90a5
sbx_user1051 <sbx_user1051@169.254.15.3>
2025-04-23 10:39:21 +0000

    Wed Apr 23 10:39:21 UTC 2025

---
+++ src/BeFaster.App/Solutions/CHK/GroupOffer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BeFaster.App.Solutions.CHK
+{
+    internal class GroupOffer : IOffer
+    {
+        public GroupOffer(int amount, int bulkBuyPrice) 
+        {
+            ItemAmount = amount;
+            BulkBuyPrice = bulkBuyPrice;
+        }
+
+        public int ItemAmount { get; }
+
+        public int BulkBuyPrice { get; }
+    }
+}

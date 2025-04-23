16691861b9601e550a42c5c7660c15454f42bd77
sbx_user1051 <sbx_user1051@169.254.37.5>
2025-04-23 09:46:47 +0000

    Wed Apr 23 09:46:47 UTC 2025

---
+++ src/BeFaster.App/Solutions/CHK/OtherItemFreeOffer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BeFaster.App.Solutions.CHK
+{
+    internal class OtherItemFreeOffer : ICrossItemOffer
+    {
+        public OtherItemFreeOffer(int itemAmount, Char otherSKU) 
+        {
+            ItemAmount = itemAmount;
+            OtherSKU = otherSKU;
+        }
+
+        public int ItemAmount { get; }
+
+        public Char OtherSKU { get; }
+    }
+}

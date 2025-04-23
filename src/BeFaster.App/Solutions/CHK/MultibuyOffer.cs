913f440f6c89f4b9929e3894f446ab5874aa31e1
sbx_user1051 <sbx_user1051@169.254.37.5>
2025-04-23 09:28:47 +0000

    Wed Apr 23 09:28:47 UTC 2025

--- src/BeFaster.App/Solutions/CHK/MultibuyOffer.cs
+++ src/BeFaster.App/Solutions/CHK/MultibuyOffer.cs
@@ -1,14 +1,27 @@
-﻿// Copyright (c) SLB 
-// SLB Private
-using System;
+﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace BeFaster.App.Solutions.CHK
 {
-    internal class MultibuyOffer
+    internal class MultibuyOffer : IOffer
     {
+        public MultibuyOffer(int itemAmount, int bulkBuyPrice)
+        {
+            Offers.Add((itemAmount, bulkBuyPrice));
+        }
+
+        public MultibuyOffer(int itemAmount1, int bulkBuyPrice1, int itemAmount2, int bulkBuyPrice2)
+        {
+            Debug.Assert(bulkBuyPrice1/itemAmount1 >  bulkBuyPrice2/itemAmount2);
+            Offers.Add((itemAmount1, bulkBuyPrice1));
+            Offers.Add((itemAmount2, bulkBuyPrice2));
+        }
+
+        public List<(int itemAmount, int bulkBuyPrice)> Offers { get; } = [];
     }
 }
+
---
+++ src/BeFaster.App/Solutions/CHK/SameItemFreeOffer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BeFaster.App.Solutions.CHK
+{
+    internal class SameItemFreeOffer
+    {
+        public SameItemFreeOffer(int itemAmount) 
+        {
+            ItemAmount = itemAmount;
+        }
+
+        public int ItemAmount { get; }
+    }
+}
